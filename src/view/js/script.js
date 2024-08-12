f44f22344864d5fa90a8fd3eac8b0571a31d327f
leivas <leivashenrique12@hotmail.com>
2024-08-12 16:40:01 -0300

    adcionando marcador do leaflet

--- src/view/js/script.js
+++ src/view/js/script.js
@@ -1,31 +1,4 @@
-let map = L.map('map', {
-    center: [-6.887698002563706, -38.56015173326553],
-    zoom: 15,
-    minZoom: 14,
-    maxZoom: 16
-});
-
-let marker = L.marker([-6.887698002563706, -38.56015173326553], {
-    draggable: true,
-}).addTo(map);
-
-map.locate();
-
-map.on('locationfound', e => {
-    marker.setLatLng(e.latlng);
-    map.setView(e.latlng);
-});
-
-map.on('click', l => {
-    marker.setLatLng(l.latlng);
-    map.setView(l.latlng);
-});
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+import {lat,lng} from './map.js';
 
 function fetchData(){
     fetch("http://localhost:3000/pedidos")
@@ -104,7 +77,7 @@ button.addEventListener('click',()=>{
     const cliente=document.getElementById('cliente').value;
     const cpf=document.getElementById('cpf').value;
     const descricaoPedido=document.getElementById('pedido').value;
-    const coordinates=[marker.getLatLng().lng, marker.getLatLng().lat];
+    const coordinates=[lat,lng];
 
     const Pedido={
         restaurante,
@@ -134,10 +107,11 @@ button.addEventListener('click',()=>{
         msg.textContent='Pedido criado com sucesso';
         }
     })
+    .then(()=>{fetchData()})
     .catch(error => {
         console.error('Erro na requisição:', error);
     });
-    fetchData();
+
 });
 
 const buttonPesquisar=document.getElementById('btn-enviar');
